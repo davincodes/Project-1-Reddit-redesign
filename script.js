aa66862b8c55d4febe3f9992203736e471f78606
davincodes <aarvinantonio@gmail.com>
2023-10-31 02:16:19 +0800

    added rightside contents

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('mobile-menu').addEventListener('click', function () {
-    var navList = document.querySelector('.nav-list');
-    navList.style.display = (navList.style.display === 'block' || navList.style.display === '') ? 'none' : 'block';
-});
